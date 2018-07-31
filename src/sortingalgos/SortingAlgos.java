c2e330ffa91e39861538120f65ce8646a1aac4e3
bradleyas <bradley.s@outlook.com>
2018-07-31 04:02:44 -0600

    Made managing which algoritms are tested much easier to manage, and made checking for correctness much more simple

--- src/sortingalgos/SortingAlgos.java
+++ src/sortingalgos/SortingAlgos.java
@@ -15,7 +15,11 @@ import java.io.IOException;
  */
 public class SortingAlgos {
 
-    private static int[] dataToSort = {5, 8, 7, 4, 2, 3, 9};
+    private static int[] dataToSort = {8,5,8,4};
+    private static String[] namesOfSorts = {"BubbleSort", "MergeSort", "QuickSort", "QuickThread", "Insertion"};
+
+    private static int[] sortsToPrint = {1};
+    private static long[] timings = new long[10];
 
     private static int[] quicksortedData;
     private static int[] mergesortedData;
@@ -23,77 +27,105 @@ public class SortingAlgos {
     private static int[] insertsortedData;
 
     private static int[] quickthreadsortedData;
-
-    private static long bubbletime;
-    private static long mergetime;
-    private static long quicktime;
-    private static long inserttime;
-    private static long quickthreadtime;
+    private static boolean compareCorrectness = false;
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
         DataGenerator.generateData();
         DataGenerator.writeDataToFile();
 
         testAlgorithms();
+
+        compareCorrectness();
+
     }
 
     public static void testAlgorithms() throws IOException {
-//        dataToSort = DataGenerator.readDataFromFile();
-//        bubbletime = System.currentTimeMillis();
-//        bubblesortedData = Bubblesort.sort(dataToSort);
-//        //printIntArray(bubblesortedData);
-//        bubbletime = System.currentTimeMillis() - bubbletime;
+        long timingStart;
+        for (int i : sortsToPrint) {
+            switch (i) {
+                case 0:
+                    dataToSort = DataGenerator.readDataFromFile();
+                    timingStart = System.currentTimeMillis();
+                    bubblesortedData = Bubblesort.sort(dataToSort);
+                    printIntArray(bubblesortedData);
+                    timings[0] = System.currentTimeMillis() - timingStart;
+                    break;
+                case 1:
+                    dataToSort = DataGenerator.readDataFromFile();
+                    timingStart = System.currentTimeMillis();
+                    mergesortedData = Mergesort.sort(dataToSort);
+                    printIntArray(mergesortedData);
+                    timings[1] = System.currentTimeMillis() - timingStart;
+                    break;
+                case 2:
+                    dataToSort = DataGenerator.readDataFromFile();
+                    timingStart = System.currentTimeMillis();
+                    quicksortedData = Quicksort.sort(dataToSort);
+                    printIntArray(quicksortedData);
+                    timings[2] = System.currentTimeMillis() - timingStart;
+                    break;
+                case 3:
+                    dataToSort = DataGenerator.readDataFromFile();
+                    timingStart = System.currentTimeMillis();
+                    quickthreadsortedData = QuickSortWithThreads.sort(dataToSort);
+                    printIntArray(quickthreadsortedData);
+                    timings[3] = System.currentTimeMillis() - timingStart;
+                    break;
+                case 4:
+                    dataToSort = DataGenerator.readDataFromFile();
+                    timingStart = System.currentTimeMillis();
+                    insertsortedData = Insertionsort.sort(dataToSort);
+                    printIntArray(insertsortedData);
+                    timings[4] = System.currentTimeMillis() - timingStart;
+                    break;
+            }
 
-//        dataToSort = DataGenerator.readDataFromFile();
-//        mergetime = System.currentTimeMillis();
-//        mergesortedData = Mergesort.sort(dataToSort);
-//        printIntArray(mergesortedData);
-//        mergetime = System.currentTimeMillis() - mergetime;
-//
-//        dataToSort = DataGenerator.readDataFromFile();
-//        quicktime = System.currentTimeMillis();
-//        quicksortedData = Quicksort.sort(dataToSort);
-//        printIntArray(quicksortedData);
-//        quicktime = System.currentTimeMillis() - quicktime;
-
-        dataToSort = DataGenerator.readDataFromFile();
-        quickthreadtime = System.currentTimeMillis();
-        quickthreadsortedData = QuickSortWithThreads.sort(dataToSort);
-        printIntArray(quickthreadsortedData);
-        quickthreadtime = System.currentTimeMillis() - quickthreadtime;
-
-//        dataToSort = DataGenerator.readDataFromFile();
-//        inserttime = System.currentTimeMillis();
-//        insertsortedData = Insertionsort.sort(dataToSort);
-//        //printIntArray(insertsortedData);
-//        inserttime = System.currentTimeMillis() - inserttime;
+        }
         printArraysSideBySide();
     }
-    public static void addToSortedListPrintout(String sortname, int[] data){
-        
-    }
-    
-    
+
     public static void printArraysSideBySide() {
-        System.out.println("BubbleSort  MergeSort   QuickSort   InsertionSort");
+        String namesOfSortsLine = "";
+        String millisecondsLine = "";
+        for (int i : sortsToPrint) {
+            namesOfSortsLine += String.format("%-12s", namesOfSorts[i]);
+            millisecondsLine += String.format("%-12s", timings[i] + "ms");
+        }
+        System.out.println(namesOfSortsLine);
+        System.out.println(millisecondsLine);
+        /*
+        for (int i = 0; i < dataToSort.length; i++) {
+            System.out.println(includeSorts(sortsToPrint, i));
+        }
+        //*/
+    }
 
-        System.out.println(String.format("%-12s", String.valueOf(bubbletime) + "ms")
-                + String.format("%-12s", String.valueOf(mergetime) + "ms")
-                + String.format("%-12s", String.valueOf(quicktime) + "ms")
-                + String.format("%-12s", String.valueOf(inserttime) + "ms"));
-
-        System.out.println();
-//        String line;
-//        for (int i = 0; i < dataToSort.length; i++) {
-//            line = String.format("%-12s", String.valueOf(bubblesortedData[i]))
-//                    + String.format("%-12s", String.valueOf(mergesortedData[i]))
-//                    + String.format("%-12s", String.valueOf(quicksortedData[i]))
-//                    + String.format("%-12s", String.valueOf(insertsortedData[i]));
-//            System.out.println(line);
-//        }
+    private static String includeSorts(int[] whichSorts, int index) {
+        String sortStrings = "";
+        for (int i : whichSorts) {
+            switch (i) {
+                case 0:
+                    sortStrings += String.format("%-12s", String.valueOf(bubblesortedData[index]));
+                    break;
+                case 1:
+                    sortStrings += String.format("%-12s", String.valueOf(mergesortedData[index]));
+                    break;
+                case 2:
+                    sortStrings += String.format("%-12s", String.valueOf(quicksortedData[index]));
+                    break;
+                case 3:
+                    sortStrings += String.format("%-12s", String.valueOf(quickthreadsortedData[index]));
+                    break;
+                case 4:
+                    sortStrings += String.format("%-12s", String.valueOf(insertsortedData[index]));
+                    break;
+            }
+        }
+        return sortStrings;
     }
 
     public static void printIntArray(int[] data) {
@@ -101,5 +133,46 @@ public class SortingAlgos {
         for (int i : data) {
             System.out.println(i);
         }
+    }
+
+    private static void compareCorrectness() {
+        for (int i : sortsToPrint) {
+            switch (i) {
+                case 0:
+                    if (!isInOrder(bubblesortedData)) {
+                        System.out.println("BUBBLE is wrong");
+                    }
+                    break;
+                case 1:
+                    if (!isInOrder(mergesortedData)) {
+                        System.out.println("MERGE is wrong");
+                    }
+                    break;
+                case 2:
+                    if (!isInOrder(quicksortedData)) {
+                        System.out.println("QUICK is wrong");
+                    }
+                    break;
+                case 3:
+                    if (!isInOrder(quickthreadsortedData)) {
+                        System.out.println("QUICKTHREAD is wrong");
+                    }
+                    break;
+                case 4:
+                    if (!isInOrder(insertsortedData)) {
+                        System.out.println("INSERT is wrong");
+                    }
+                    break;
+            }
+        }
+    }
+
+    private static boolean isInOrder(int[] data) {
+        for (int i = 0, j = 1; j < data.length; i++, j++) {
+            if (data[i] > data[j]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
